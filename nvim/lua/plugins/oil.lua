1177f5977590be00d11cc7cf1cca3a61646d5bb6
Morita <morit958@icloud.com>
2026-06-09 21:54:19 +0900

    change oil keymap

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -4,6 +4,39 @@ return {
     columns = { "icon" },
     watch_for_changes = true,
     keymaps = {
+      ["l"] = {
+        callback = function()
+          local oil = require("oil")
+          local entry = oil.get_cursor_entry()
+          if entry and entry.type == "directory" then
+            oil.select()
+          end
+        end,
+        mode = "n",
+        desc = "Navigate into directory",
+      },
+      ["h"] = {
+        callback = function()
+          local git = require("oil.git")
+          local current_dir = require("oil").get_current_dir()
+
+          if not current_dir then
+            return
+          end
+
+          current_dir = current_dir:gsub("/$", "")
+
+          local git_root = git.get_root(current_dir)
+          local root = (git_root or vim.fn.getcwd()):gsub("/$", "")
+
+          if current_dir ~= root then
+            require("oil.actions").parent.callback()
+          end
+        end,
+        mode = "n",
+        desc = "Navigate to parent",
+      },
+      ["<CR>"] = "actions.select",
       ["-"] = {
         callback = function()
           local git = require("oil.git")
@@ -44,7 +77,7 @@ return {
   },
   lazy = false,
 
-  vim.keymap.set("n", "-", function()
+  vim.keymap.set("n", "h", function()
     vim.cmd("Oil")
   end, { desc = "Open Oil" }),
 }
