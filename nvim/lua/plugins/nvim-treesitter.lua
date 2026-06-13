bec96367e86d9231aa211fcbe9072019100535cc
Morita <morit958@icloud.com>
2026-06-13 23:09:48 +0900

    enable treesitter auto start

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,4 +2,12 @@ return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
   build = ":TSUpdate",
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function(ev)
+        pcall(vim.treesitter.start, ev.buf)
+      end,
+    })
+  end,
 }
