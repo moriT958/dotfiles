5fca90d21e5c5f45e758ed6f8b74443a87e412c6
Morita <morit958@icloud.com>
2026-07-22 21:26:02 +0900

    (neovim): Change color theme

--- .config/nvim/lua/plugins/color-theme.lua
+++ .config/nvim/lua/plugins/color-theme.lua
@@ -1,21 +1,9 @@
 local gh = require("config.gh")
 
-vim.pack.add({ gh("catppuccin/nvim") })
+vim.pack.add({ gh("navarasu/onedark.nvim") })
 
-require("catppuccin").setup({
-  transparent_background = true,
-  default_integrations = true,
-  integrations = {
-    cmp = true,
-    gitsigns = true,
-    neotree = true,
-    treesitter = true,
-  },
-  custom_highlights = function(colors)
-    return {
-      LineNr = { fg = colors.overlay0 },
-    }
-  end,
+require("onedark").setup({
+  style = "dark",
+  transparent = true,
 })
-vim.cmd.colorscheme("catppuccin-frappe")
--- vim.cmd.colorscheme("catppuccin-latte")
+require("onedark").load()
